1611d2ae6fe227736d4e3ea5c31be7c020438ee5
onurbilginnn <onurbilginnn@gmail.com>
2023-01-30 16:54:21 +0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -6,6 +6,10 @@ describe('Deterministic Partition Key Tests', () => {
       const result = deterministicPartitionKey();
       expect(result).toBe(TRIVIAL_PARTITION_KEY);
     });
+        it('should return crypto string', () => {
+      const result = deterministicPartitionKey({partitionKey: '123'});
+      expect(result).toBe(crypto.createHash(HASH_TYPE).update('123').digest(DIGEST_TYPE));
+    });
 
   });
   
